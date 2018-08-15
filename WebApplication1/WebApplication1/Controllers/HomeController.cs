17191f52ea420dbde74ec3b7af1bad8ee7fecc21
elamathiav <elamathi@gmail.com>
2018-08-15 16:38:30 +1000

    Checking in to see the CI/CD working

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "My First Azure application for CI/CD";
 
 			return View();
 		}
